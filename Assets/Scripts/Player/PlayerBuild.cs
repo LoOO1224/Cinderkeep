cd8abda46cdeabd9ea9393704fce2970c8e89b2d
jaeho <>
2026-06-17 17:37:02 +0900

    PlayerBuild.cs 작성 / Player 앞에 건축물 프리팹 소환 구현

--- Assets/Scripts/Player/PlayerBuild.cs
+++ Assets/Scripts/Player/PlayerBuild.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerBuild : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject Prefab_Fence;
+    [SerializeField] private float SpawnDistance = 3.0f;
+
+    private void Update()
     {
-        
+        HandleInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleInput()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBuilding();
+        }
+    }
+
+    private void SpawnBuilding()
+    {
+        if(Prefab_Fence == null)
+        {
+            Debug.LogError("Prefab_Fence가 인스펙터에 할당되지 않았습니다");
+            return;
+        }
+
+        Vector3 spawnPosition = transform.position + (transform.forward * SpawnDistance);
+
+        Quaternion spawnRotation = transform.rotation;
+
+        Instantiate(Prefab_Fence, spawnPosition, spawnRotation);
+
+        Debug.Log($"캐릭터 정면 {SpawnDistance}m 앞에 건축물이 생성되었습니다");
     }
 }
