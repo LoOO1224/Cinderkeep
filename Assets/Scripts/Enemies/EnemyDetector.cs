64b05ae275920a338b4b8a642c1d764d128a8118
wlwodnr <wlwodnr0111@gmail.com>
2026-06-17 16:37:07 +0900

    적 오브젝트가 캐릭터 오브젝트 탐지 로직 만드중
    플레이어 태그를 갖고있는 오브젝트가 적 오브젝트로부터 원 안에 있는지, 혹은 부채꼴 시야각 내에 들어와 있는지 검사.
    만약, 원 안에 있는데 부채꼴에 없다면, 플레이어가 공격하기 전까진, 어그로가 끌리지 않도록
    맞았을 때 시야각이 아닌, 구형 범위 내의 채크하는 기능 구현 예정

--- Assets/Scripts/Enemies/EnemyDetector.cs
+++ Assets/Scripts/Enemies/EnemyDetector.cs
@@ -1,16 +1,80 @@
-using UnityEngine;
+﻿using UnityEngine;
+using Cinderkeep.Gameplay;
 
 public class EnemyDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _viewAngle = 90.0f;
+
+    private const string TargetTag = "Player";
+    private const int MaxAllocation = 20;   //객체가 감지하는 오브젝트의 최대 갯수
+
+    private readonly Collider[] _overlapColliders = new Collider[MaxAllocation];    //감지된 객체의 배열
+
+    private float _detectorDistance;
+
+    public Transform Transform_DetectedPlayer { get; private set; }
+
+    public bool _isHashDetectedPlayer
     {
-        
+        get
+        {
+            return _isHashDetectedPlayer != null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PerformDetection();
     }
+
+
+    public void Initialize(EnemyData enemyData)
+    {
+        if(enemyData == null)
+        {
+            return;
+        }
+        _detectorDistance = enemyData.DetectorDistance;
+    }
+
+    private void PerformDetection()
+    {
+        if(_isHashDetectedPlayer)
+        {
+            return ;
+        }
+
+        int hitCount = Physics.OverlapSphereNonAlloc(transform.position, _detectorDistance, _overlapColliders);
+        Transform transformTargetFound = null;
+
+        for(int i = 0; i < hitCount; i++)
+        {
+            Collider col = _overlapColliders[i];
+            if(col.transform == transform)
+            {
+                continue;
+            }
+            if(!col.CompareTag(TargetTag))
+            {
+                continue;
+            }
+
+            if(IsInSectorFieldOfView(col.transform))
+            {
+                transformTargetFound = col.transform;
+                break;
+            }
+        }
+        Transform_DetectedPlayer = transformTargetFound;
+    }
+    private bool IsInSectorFieldOfView(Transform transformTarget)
+    {
+        Vector3 directionToTarget = (transformTarget.position - transform.position).normalized;
+
+        float angle = Vector3.Angle(transform.forward, directionToTarget);
+
+        return angle <= (_viewAngle * 0.5f);
+    }
+
+
 }
