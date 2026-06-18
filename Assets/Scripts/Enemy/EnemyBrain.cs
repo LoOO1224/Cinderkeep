9460f2422ff623be6356d0de16d555e1e7eb1f0d
SHKwon <gkrrpdl@gmail.com>
2026-06-18 17:52:26 +0900

    [몬스터AI] 공격 로직 구현
    EnemyBrain.cs 파일을 만들었습니다.
    Assets/Scripts/Enemy/EnemyBrain 경로로 만들었습니다.
    
    작업내용:
    EnemyAttack 컴포넌트를 받아옵니다.
    DecideAttackTarget 함수 로직 설명:
    1. Damageable컴포넌트를 타겟에서 받아옵니다.
    2. 타겟의 태그를 구분합니다 (Player, Build)
    3. 태그가 일치하면 EnemyAttack 클래스의 TryAttack() 함수를 불러옵니다.
    
    - 추가사항: 공격성공 여부를 판단하기 위해 멤버변수로 bool값 _attacked를 추가하였습니다.
    
    - 주의사항: 콜라이더 점검을 EnemyDetector에서 할 예정이라고 해서 추후 불러와서 공격 조건 판단할 예정입니다.

---
+++ Assets/Scripts/Enemy/EnemyBrain.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using Cinderkeep.Gameplay;
+
+public class EnemyBrain : MonoBehaviour
+{
+    private EnemyAttack _enemyAttack;
+    private EnemyMovement _enemyMovement;   // 추후 이동 판단 관련 추가 가능성 열어둠
+    private Damageable _currentTarget;
+    private bool _attacked;
+
+    public void Awake()
+    {
+        _enemyAttack = GetComponent<EnemyAttack>();
+        _enemyMovement = GetComponent<EnemyMovement>();
+    }
+
+    // 추후 EnemyDetector의 거리계산 로직 불러와서 사용할 예정
+    private void EnemyDetectorDistanceLogic()
+    {
+        if (_currentTarget == null) return;
+
+        DecideAttackTarget(_currentTarget.gameObject);
+
+        if (_attacked)
+        {
+            // 공격 성공 시 로직
+        }
+    }
+
+    private void DecideAttackTarget(GameObject targetObject)
+    {
+        if (targetObject == null) return;
+
+        var damageable = targetObject.GetComponent<Damageable>();
+        if (damageable == null) return;
+
+        if (targetObject.CompareTag("Build"))
+        {
+            if (_enemyAttack.TryAttack(damageable))
+            {
+                _attacked = true;
+            }
+        }
+
+        else if (targetObject.CompareTag("Player"))
+        {
+            if (_enemyAttack.TryAttack(damageable))
+            {
+                _attacked = true;
+            }
+        }
+
+        else
+        {
+            _attacked = false;
+        }
+    }
+}
