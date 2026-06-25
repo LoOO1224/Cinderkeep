97983f02435344ad3a39250c55e63803f6add635
wlwodnr <145469443+wlwodnr@users.noreply.github.com>
2026-06-25 16:30:22 +0900

    EnemyMoveToTargetAction 준비.
    초기 세팅. 컴포넌트 체크

--- Assets/Behavior/Enemy/EnemyMoveToTargetAction.cs
+++ Assets/Behavior/Enemy/EnemyMoveToTargetAction.cs
@@ -1,16 +1,64 @@
+﻿using System;
+using Unity.Behavior;
+using Unity.Properties;
 using UnityEngine;
+using Action = Unity.Behavior.Action;
 
-public class EnemyMoveToTargetAction : MonoBehaviour
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(
+    name: "Enemy Move To Target",
+    story: "[Self] moves to [Target] until [ArriveDistance]",
+    category: "Action/Cinderkeep/Enemy",
+    id: "cinderkeep_enemy_move_to_target_action_v1"
+    )]
+public class EnemyMoveToTargetAction : Action
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<float> ArriveDistance = new BlackboardVariable<float>(0.3f);
+
+    private EnemyMovement _enemyMovement;
+
+    protected override Status OnStart()
     {
-        
+        GameObject selfObject = GetSelfObject();
+        if(IsUnityObjectNull(selfObject))
+        {
+            LogFailure("EnemyMoveToTargetAction : Self가 없습니다.", true);
+            return Status.Failure;
+        }
+
+        if(IsUnityObjectNull(Target == null ? null : Target.Value))
+        {
+            LogFailure("EnemyMoveToTargetAction : Target이 없습니다", false);
+            return Status.Failure;
+        }
+
+        _enemyMovement = selfObject.GetComponent<EnemyMovement>();
+        if(_enemyMovement == null)
+        {
+            LogFailure("EnemyMoveToTargetAction: Self에 EnemyMovement가 없습니다.", true);
+            return Status.Failure;
+        }
+
+        return Status.Running;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject GetSelfObject()
     {
-        
+        if(Self == null && IsUnityObjectNull(Self.Value) == false)
+        {
+            return Self.Value;
+        }
+        return GameObject;
     }
+
+
+    private static bool IsUnityObjectNull(UnityEngine.Object targetObject)
+    {
+        return ReferenceEquals(targetObject, null) || targetObject == null;
+    }
+
 }
